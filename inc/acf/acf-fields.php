87c38f3a033be33e24f325fb31c531b740273ca3
Guillaume Marcel <55285167+marcelguillaume@users.noreply.github.com>
2024-09-16 11:29:05 +0200

    Add local block ACF support

--- inc/acf/acf-fields.php
+++ inc/acf/acf-fields.php
@@ -18,16 +18,83 @@ if (function_exists('acf_add_options_page')) {
  * ACF JSON
  */
 
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+// Save ACF Json in inc/acf/json folder
+add_filter('acf/settings/save_json', 'my_acf_json_save_point', 10);
 function my_acf_json_save_point($path)
 {
     $path =  get_stylesheet_directory() . '/inc/acf/json';
     return $path;
 }
 
+// Load ACF Json in block folder && global folder
 add_filter('acf/settings/load_json', 'my_acf_json_load_point');
 function my_acf_json_load_point($paths)
 {
+    $loader = new Skouerr_Loader();
+    $blocks = $loader->get_blocks();
+
+    foreach ($blocks as $block) {
+        $dir = dirname($block);
+        $acf_dir = $dir . '/acf-json';
+        if (is_dir($acf_dir)) {
+            $paths[] = $acf_dir;
+        }
+    }
+
     $paths[] = get_stylesheet_directory() . '/inc/acf/json';
     return $paths;
 }
+
+// Save ACF Json in block folder
+function custom_acf_json_save_paths_block($paths, $post)
+{
+    $locations = $post['location'];
+    $is_block = false;
+    $block = '';
+    $block_name = '';
+
+    foreach ($locations as $location) {
+        if ($location[0]['param'] === 'block' && $location[0]['operator'] === '==') {
+            $is_block = true;
+            $block = $location[0]['value'];
+        }
+    }
+
+    if (isset($block)) {
+        $blocks_names = explode('/', $block);
+        $block_name = end($blocks_names);
+    }
+
+    if ($is_block && !empty($block_name)) {
+        $path = get_stylesheet_directory() . '/blocks/' . $block_name . '/acf-json';
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+        $paths = array($path);
+    }
+
+    return $paths;
+}
+add_filter('acf/json/save_paths', 'custom_acf_json_save_paths_block', 10, 2);
+
+
+
+function custom_acf_json_filename($filename, $post, $load_path)
+{
+    $filename = str_replace(
+        array(
+            ' ',
+            '_',
+        ),
+        array(
+            '-',
+            '-'
+        ),
+        $post['title']
+    );
+
+    $filename = strtolower($filename) . '.json';
+
+    return $filename;
+}
+add_filter('acf/json/save_file_name', 'custom_acf_json_filename', 10, 3);
